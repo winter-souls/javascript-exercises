56e0f425089362154b8f9ecf13e468d3a973573d
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-05-13 10:38:17 -0700

    Create repeatString loop
    Take detailed notes on how it works in comments

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR"; //throws an error if times is 0
+    let string = ""; //establishes empty string
+    for (let i = 0; i < times; i++ ) {
+    //i is 0; as long as i < times, add one i 
+        string += word;
+        //# of i's = # of words inserted into the string variable
+    }
+    return string; //return the new string with repeated words
 
 };
 
