65f659acde928632d07678786dc6cb7082f35fad
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-05-13 10:44:22 -0700

    Add clarification note
    Enable tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,13 +1,12 @@
 const repeatString = function(word, times) {
     if (times < 0) return "ERROR"; //throws an error if times is 0
-    let string = ""; //establishes empty string
+    let string = ""; //establishes empty string variable to store words in
     for (let i = 0; i < times; i++ ) {
     //i is 0; as long as i < times, add one i 
         string += word;
         //# of i's = # of words inserted into the string variable
     }
     return string; //return the new string with repeated words
-
 };
 
 // Do not edit below this line
