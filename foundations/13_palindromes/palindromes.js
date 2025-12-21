015e06e2929ecd4c11937d60c7aaa1ece2548e1a
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-12-21 16:39:44 -0700

    Create a constant that holds all letters and #s

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Constant that holds all letters and numbers
+    let alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 };
 
 // Do not edit below this line
