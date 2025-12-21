8b597bf9461c0628f88f3e8b13ed7daae363842f
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-12-21 16:43:37 -0700

    Clean up the string

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
 const palindromes = function (string) {
     // Constant that holds all letters and numbers
     let alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+    //Clean up the string
+    const cleanedString = string
+    .toLowerCase() //make all letters lowercase
+    .split('') //split all characters into an array
+    .filter((character) => alphanumerical.includes(character)) //filter array to only include characters/numbers
+    .join(''); //join array back into a string
 };
 
 // Do not edit below this line
