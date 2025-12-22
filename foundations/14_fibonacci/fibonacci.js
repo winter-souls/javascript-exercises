93efaeb9646d39e9372c85c625dc970f7f343520
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-12-22 12:39:58 -0700

    Create + fill fibonacci array up to user-entered #
    Return user-requested position in arrays

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(requestedNumber) {
+    // Create an array of the fibonacci sequence
+    let fibonacci = [0, 1];
 
+    // Loop through array, adding numbers in the sequence
+    for (let i = 2; i <= requestedNumber; i++) {
+        fibonacci[i] = fibonacci[i -1] + fibonacci[i-2];
+    }
+    // Find the user-requested position in that array
+    return fibonacci[requestedNumber];
 };
 
 // Do not edit below this line
