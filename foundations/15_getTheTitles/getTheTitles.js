145bebad4df50fd8d3c4b3af12da2ef2b7adffe3
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-12-22 13:15:24 -0700

    Map over books objects and get titles
    Use map method because it performs a function on each item and returns a new array

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    // Use map method to iterate over array items
+    // Map method performs a function using each item as its argument ("book")
+    // The function gets each book's title and returns it
+    return array.map((book) => book.title);
 };
 
 // Do not edit below this line
